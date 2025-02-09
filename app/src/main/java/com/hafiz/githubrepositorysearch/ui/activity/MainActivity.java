b9d30604b7d3c50e5dab20c112fd9ac54bd378d4
hafizalasadchy <hafizalasadchy@gmail.com>
2025-02-09 19:48:13 +0600

    Icon & appname changed

--- app/src/main/java/com/hafiz/githubrepositorysearch/ui/activity/MainActivity.java
+++ app/src/main/java/com/hafiz/githubrepositorysearch/ui/activity/MainActivity.java
@@ -1,13 +1,10 @@
 package com.hafiz.githubrepositorysearch.ui.activity;
 
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
-
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -15,9 +12,6 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.hafiz.githubrepositorysearch.R;
 import com.hafiz.githubrepositorysearch.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     private AppBarConfiguration appBarConfiguration;
@@ -46,15 +40,13 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
